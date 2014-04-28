5fda086132735fe7bba0ad7f595782cb90daa30d
zetway <zeway@mail.ru>
2014-04-28 09:29:27 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+<head>		
+	<title>Trackr :: Main Page</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<script type="text/javascript" src="js/jquery2.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+	<h1>Nice Heading</h1>
+	<ul class="nav nav-pills">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+</body>
+</html>
